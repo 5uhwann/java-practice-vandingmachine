9b60bf858dda9908232fe9d02269cd9e40ba09a4
5uhwann <5uhwann27@gmail.com>
2022-12-06 18:33:14 +0900

    feat: 잔돈 금액 반환 구현

--- src/main/java/vendingmachine/domain/calculator/Calculator.java
+++ src/main/java/vendingmachine/domain/calculator/Calculator.java
@@ -1,7 +1,13 @@
 package vendingmachine.domain.calculator;
 
+import java.util.Optional;
+import vendingmachine.domain.moneymanager.MoneyBox;
+import vendingmachine.domain.productmanager.Products;
+
 public interface Calculator {
 
     int calculate(int inputAmount, int subtractedPrice);
+
+    Optional<Integer> payChange(MoneyBox moneyBox, Products products, String productName);
 
 }
--- src/main/java/vendingmachine/domain/calculator/CalculatorImpl.java
+++ src/main/java/vendingmachine/domain/calculator/CalculatorImpl.java
@@ -1,9 +1,36 @@
 package vendingmachine.domain.calculator;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import vendingmachine.domain.moneymanager.MoneyBox;
+import vendingmachine.domain.productmanager.Product;
+import vendingmachine.domain.productmanager.Products;
+
 public class CalculatorImpl implements Calculator{
 
     @Override
     public int calculate(int inputAmount, int subtractedPrice) {
         return inputAmount - subtractedPrice;
+    }
+
+    @Override
+    public Optional<Integer> payChange(MoneyBox moneyBox, Products products, String productName) {
+        Optional<Product> product = products.findProductByName(productName);
+        if (product.isEmpty()) {
+            throw new IllegalArgumentException("[ERROR] 존재하지 않는 상품입니다.");
+        }
+        if (products.getMinProductPrice() > moneyBox.getInputAmount() || isSoldOut(products)) {
+            return Optional.of(moneyBox.getInputAmount());
+        }
+        return Optional.empty();
+    }
+
+    private boolean isSoldOut(Products products) {
+        List<Product> soldOutProducts = products.getProducts().stream()
+                .filter(product -> product.getQuantity() == 0)
+                .collect(Collectors.toList());
+
+        return soldOutProducts.size() == products.getProducts().size();
     }
 }
