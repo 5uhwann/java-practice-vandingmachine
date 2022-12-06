4281a1379cb858087c69fc19868ebe5de280c0d8
5uhwann <5uhwann27@gmail.com>
2022-12-06 21:56:14 +0900

    feat: 사용자 입력 및 출력 메시지 출력 구현

---
+++ src/main/java/vendingmachine/view/input/InputView.java
@@ -0,0 +1,11 @@
+package vendingmachine.view.input;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    public String userInput() {
+        return Console.readLine();
+    }
+
+}
---
+++ src/main/java/vendingmachine/view/output/OutputView.java
@@ -0,0 +1,9 @@
+package vendingmachine.view.output;
+
+public class OutputView {
+
+    public void printMessage(String outputMessage) {
+        System.out.println(outputMessage);
+    }
+
+}
